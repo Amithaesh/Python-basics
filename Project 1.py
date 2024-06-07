40e9d98d85e67b9fa9baa05fcd61651448e3a013
Amithaesh <bala.dr16@gmail.com>
2024-06-07 17:21:03 +0530

    Add files via upload

---
+++ Project 1.py
@@ -0,0 +1,11 @@
+'''Colour=input('Enter the colour')
+if(Colour=='Red'):
+    print('Stop')
+elif(Colour=='Green'):
+    print('you can go')
+elif(Colour=='yellow'):
+    print('Slow down and wait')
+else:
+    print('Invalid colour')'''
+
+
